19547c6290f745294f45b02a7eea62cd521c68e1
Prapanch <prapanchr555@gmail.com>
2026-05-12 17:14:55 +0000

    Daily auto problem: Tue May 12 17:14:55 UTC 2026

--- 2026-05-12.py
+++ 2026-05-12.py
@@ -1,16 +1,3 @@
-# Problem: Count vowels in string
+# Problem: Find duplicate in array
 
 # Write your solution here
-def count_vowels(s):
-    vowels = "aeiouAEIOU"
-    count = 0
-
-    for ch in s:
-        if ch in vowels:
-            count += 1
-
-    return count
-
-
-text = "hello world"
-print(count_vowels(text))
