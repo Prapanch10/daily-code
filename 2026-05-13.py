24b132d1d01ff17e52c7207269b17076d20e75e7
Prapanch R <prapanchr555@gmail.com>
2026-05-14 10:59:21 +0530

    Implement palindrome check function
    Added a function to check if a string is a palindrome.

--- 2026-05-13.py
+++ 2026-05-13.py
@@ -1,3 +1,14 @@
 # Problem: Check palindrome string
 
 # Write your solution here
+def check(name):
+    left = 0
+    right = len(name) - 1
+
+    while left < right:
+        if name[left] != name[right]:
+            return False
+        left += 1
+        right -= 1
+
+    return True
