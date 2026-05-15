c4a1f509050e79f2b4b184243e08fef86772d3b0
Prapanch R <prapanchr555@gmail.com>
2026-05-15 09:03:25 +0530

    Implement string reversal function

--- 2026-05-14.py
+++ 2026-05-14.py
@@ -1,3 +1,12 @@
 # Problem: Reverse a string
 
 # Write your solution here
+def reverse(name):
+  
+  new_name = ""
+  for i in range(len(name)-1,-1,-1):
+    new_name += name[i]
+  return new_name
+
+name = "hello"
+print(reverse(name))
